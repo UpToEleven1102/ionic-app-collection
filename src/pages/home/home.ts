8b687712e4bd6309e4ac951a477b18e6e98261ca
Huyen Vu <huyen.vu@nextgencode.io>
2019-01-26 16:31:42 -0600

    add functions to button delete, archive items

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  todos = [];
+  archived = [];
+
+  constructor(public navCtrl: NavController, private alertController: AlertController) {
 
   }
 
+  openAddTodoAlert() {
+    const addTodoAlert = this.alertController.create({
+      title: 'Add new Todo',
+      message: 'Enter your Todo',
+      inputs: [
+        {
+          name: 'addTodoInput',
+          type: 'text',
+          placeholder: 'Todo',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log(data)
+          }
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            if (data.addTodoInput.length > 0) {
+              this.todos.push(data.addTodoInput)
+            }
+          }
+        },
+      ],
+    });
+
+    addTodoAlert.present().then(res => console.log(res)). catch(err => console.log(err));
+  }
+
+  removeTodoItem(item) {
+    const idx = this.todos.indexOf(item)
+    this.todos.splice(idx, 1)
+  }
+
+  archiveTodoItem(item) {
+    this.archived.push(item);
+    this.removeTodoItem(item);
+
+    console.log('archived: ', this.archived)
+  }
 }
