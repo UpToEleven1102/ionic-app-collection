1bb06b666228aa0e360192356dc72e89a1af067e
Huyen Vu <huyen.vu@nextgencode.io>
2019-01-27 01:02:00 -0600

    ionic generate provider music

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {MusicProvider} from "../../providers/music/music";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  music = []
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private musicService: MusicProvider) {
+    this.musicService.getMusic().subscribe({next: value => this.music.push(value), error: err => console.warn(err)})
   }
 
 }
---
+++ src/providers/music/music.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the MusicProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+
+const apiUrl = 'https://orangevalleycaa.org/api/music'
+
+@Injectable()
+export class MusicProvider {
+  constructor(public http: HttpClient) {
+  }
+
+  getMusic() {
+    return this.http.get(apiUrl)
+  }
+}
