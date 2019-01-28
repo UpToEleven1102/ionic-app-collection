2888c92fbe2d3a7ebfe4075df38ef74a9890e777
Huyen Vu <huyen.vu@nextgencode.io>
2019-01-27 21:48:37 -0600

    add loadingController

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import {MusicProvider} from "../../providers/music/music";
 
 @Component({
@@ -9,13 +9,18 @@ import {MusicProvider} from "../../providers/music/music";
 export class HomePage {
   allMusic
 
-  constructor(public navCtrl: NavController, private musicService: MusicProvider) {
+  constructor(public navCtrl: NavController, private musicService: MusicProvider, private loadingCtrl: LoadingController) {
 
   }
 
   ionViewDidLoad(){
+    const loadingModal = this.loadingCtrl.create({content: "Getting Music"})
+
+    loadingModal.present()
+
     this.musicService.getMusic().then(music => {
       this.allMusic = music
+      loadingModal.dismissAll()
     })
   }
 }
