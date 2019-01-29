e5299d07059c77ef3dc42a68613200991f779025
Huyen Vu <huyen.vu@nextgencode.io>
2019-01-28 20:45:02 -0600

    done chess board

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+interface Coord {
+    x: number;
+    y: number;
+}
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+    selector: 'app-board',
+    templateUrl: './board.component.html',
+    styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+    sixtyFour = new Array(64).fill(0).map((_, i) => i);
 
-  constructor() { }
+    xy(i): Coord {
+        return {
+            x: i % 8,
+            y: Math.floor(i / 8)
+        };
+    }
 
-  ngOnInit() {
-  }
+    isBlack({x, y}: Coord) {
+        return (x + y) % 2 === 1;
+    }
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+    }
 
 }
